5e8738acfd7b875c18943be6344f1c9ce47e075d
Santiago Rosado <srosado@lcs.on.ca>
2022-01-20 09:44:22 -0500

    I got my two loops working

--- Playground.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -12,14 +12,19 @@ PlaygroundPage.current.liveView = canvas
 
 
 
-for horizontalPosition in stride(from: 0, through: 450, by: 50) {
+for verticalPosition in stride(from: 0, through: 450, by: 50) {
     
-    horizontalPosition
+    for horizontalPosition in stride(from: 0, through: 450, by: 50) {
+        canvas.drawEllipse(at: Point(x: horizontalPosition + 25,
+                                     y: verticalPosition + 25),
+                           width: 50,
+                           height: 50)
+    }
+            
+            
     
-    canvas.drawEllipse(at: Point(x: horizontalPosition + 25, y: 25), width: 50, height: 50)
 
 }
 
 
-        
-canvas.drawAxes(withScale: true, by: 50, color: .black)
+
